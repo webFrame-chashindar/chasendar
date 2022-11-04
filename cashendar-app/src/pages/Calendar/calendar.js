2fca4edada742af9a107853d5361bcd027d1d33f
yootal <khyoo96@naver.com>
2022-11-04 12:11:39 +0900

    간단한 calendar.css추가

--- cashendar-app/src/pages/Calendar/calendar.js
+++ cashendar-app/src/pages/Calendar/calendar.js
@@ -2,22 +2,27 @@
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
 
-function Calendar(){
-    return (
-        <div>
-          <FullCalendar
-        plugins={[ dayGridPlugin ]}
-        initialView="dayGridMonth" 
-        height = '100vh' // 세로 길이
-        locale = 'ko' // 한국어 설정
-        headerToolbar= {{ // 툴바 위치 설정
+function Calendar() {
+  return (
+    <div>
+      <div id="FullCalendar">
+        <FullCalendar
+          plugins={[dayGridPlugin]}
+          initialView="dayGridMonth"
+          height='100vh' // 세로 길이
+          locale='ko' // 한국어 설정
+          headerToolbar={{ // 툴바 위치 설정
             left: 'title',
             center: '',
             right: 'today prev,next'
           }}
         />
-        </div>
-    )
+      </div>
+      <div id="Detail">
+        세부사항
+      </div>
+    </div>
+  )
 }
 
 export default Calendar;
