e804cc1f79530376e492fc21a97acf1ae5c16460
yootal <khyoo96@naver.com>
2022-11-04 13:29:27 +0900

    캘린더 색 변경

--- cashendar-app/src/pages/Calendar/calendar.js
+++ cashendar-app/src/pages/Calendar/calendar.js
@@ -1,6 +1,7 @@
 //import React, { useState } from 'react'
 import FullCalendar from '@fullcalendar/react'
 import dayGridPlugin from '@fullcalendar/daygrid'
+import "./calendar.css"
 
 function Calendar() {
   return (
