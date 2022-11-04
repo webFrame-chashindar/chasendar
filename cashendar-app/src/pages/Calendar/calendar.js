db5d7e92935c8c0ffc8720523104c4c028d74ca2
yootal <khyoo96@naver.com>
2022-11-04 11:39:10 +0900

    Update calendar.js
    기본 캘린더

--- cashendar-app/src/pages/Calendar/calendar.js
+++ cashendar-app/src/pages/Calendar/calendar.js
@@ -1,7 +1,23 @@
-import React from "react";
+//import React, { useState } from 'react'
+import FullCalendar from '@fullcalendar/react'
+import dayGridPlugin from '@fullcalendar/daygrid'
 
-function Calendar() {
-    return <>This is Calendar.js</>;
+function Calendar(){
+    return (
+        <div>
+          <FullCalendar
+        plugins={[ dayGridPlugin ]}
+        initialView="dayGridMonth" 
+        height = '100vh' // 세로 길이
+        locale = 'ko' // 한국어 설정
+        headerToolbar= {{ // 툴바 위치 설정
+            left: 'title',
+            center: '',
+            right: 'today prev,next'
+          }}
+        />
+        </div>
+    )
 }
 
 export default Calendar;
