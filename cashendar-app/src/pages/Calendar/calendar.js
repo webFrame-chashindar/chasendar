73a9e3c148bd8d2053150b91dab9ded4cd333775
yootal <khyoo96@naver.com>
2022-11-12 02:03:28 +0900

    시간 오차 수정 + 일정에 색 적용

--- cashendar-app/src/pages/Calendar/calendar.js
+++ cashendar-app/src/pages/Calendar/calendar.js
@@ -13,17 +13,17 @@ var d = new Date(); // 출력형태 Tue Feb 07 2020 23:25:32 GMT+0900 (KST)
 var Events = []
 
 // 더미 (일정)이벤트 목록
-var dummyScheduleEvents = [
-  { title: '머리 자르기', date: '2022-11-07' },
-  { title: '중간고사 공부하기', date: '2022-11-07' },
-  { title: 'event 1', date: '2022-11-22' },
-  { title: 'event 2', date: '2022-11-22' },
-  { title: 'event 3', date: '2022-11-22' },
-  { title: 'event 4', date: '2022-11-09' },
-  { title: 'event 5', date: '2022-11-09' },
-  { title: 'event 6', date: '2022-11-09' },
-  { title: 'event 7', date: '2022-11-09' }
-]
+// var dummyScheduleEvents = [
+//   { title: '머리 자르기', date: '2022-11-07' },
+//   { title: '중간고사 공부하기', date: '2022-11-07' },
+//   { title: 'event 1', date: '2022-11-22' },
+//   { title: 'event 2', date: '2022-11-22' },
+//   { title: 'event 3', date: '2022-11-22' },
+//   { title: 'event 4', date: '2022-11-09' },
+//   { title: 'event 5', date: '2022-11-09' },
+//   { title: 'event 6', date: '2022-11-09' },
+//   { title: 'event 7', date: '2022-11-09' }
+// ]
 
 // 더미 (수입과 지출)이벤트 목록
 var dummyInOutEvents = [
@@ -45,35 +45,38 @@ var dummyInOutEvents = [
 function Calendar({user}) {
   // 날짜 상태 (오늘의 날짜를 초기값) + 한국 기준 시간으로 변경
   const [ScheduleDate, setScheduleDate] = useState(new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString().slice(0, 10));
-  //const dateeee = new Date(d.getTime() - (d.getTimezoneOffset() * 60000)).toISOString().slice(0, 10)
-  //console.log(d);
-  //console.log(dateeee);
   const [SelectCalendar, setSelectCalendar] = useState(1); // 1 -> 일정 캘린더로 시작
   const [eventList, setEventList] = useState([]);
 
   // 선택한 모드에 따라 출력할 캘린더 변경
   if (SelectCalendar === 1) {
-    Events = dummyScheduleEvents;
+    Events = eventList;
   }
   else {
     Events = dummyInOutEvents;
   }
 
-  // 
   const planCollection = collection(db, "plan");
   useEffect(() => {
     const getPlan = async () => {
       const data = await getDocs(planCollection);
       //console.log(data);
       const dataList = data.docs.map((doc)=>({ ...doc.data(), id: doc.id}))
-      //const filterList = eventList.filter(value=>value.user === {user});
-      const PlanList = dataList.map((value)=>({title: value.title, date: value.startDate.toDate().toISOString().slice(0,10), start: value.startDate.toDate().toISOString().slice(0,10), end: value.endDate.toDate().toISOString().slice(0,10), user: value.user}))
-
+      const PlanList = dataList.map(
+        (value)=>({
+          title: value.title, 
+          start: (value.startDate).toDate().toISOString(), 
+          end: (value.endDate).toDate().toISOString(), 
+          startD: ((value.startDate).toDate().setHours((value.startDate).toDate().getHours()+9)),
+          endD: ((value.endDate).toDate().setHours((value.endDate).toDate().getHours()+9)),
+          user: value.user,
+          backgroundColor: value.color,
+          borderColor : value.color
+        }))
       console.log(PlanList); 
       setEventList(PlanList.filter(value => value.user === user));
       console.log(eventList);
     };
-
    getPlan();
   },[]);
 
@@ -96,8 +99,8 @@ function Calendar({user}) {
           }}
           // 달력에 표시될 캘린더
           events={
-            //Events
-            eventList
+            Events
+            // eventList
           }
           selectable='true'
           dayMaxEvents='true' // 달력에 나올 이벤트 갯수 제한
@@ -129,10 +132,13 @@ function Calendar({user}) {
         <div id="TodaySchedule">
           <h3 id="daySchedule">일정</h3>
           <ul>
-            {dummyScheduleEvents.filter(item => item.date === ScheduleDate)
+            {eventList.filter(item => 
+              ((new Date(item.startD)).toISOString().slice(0,10) <= ScheduleDate && 
+              (new Date(item.endD)).toISOString().slice(0,10) >= ScheduleDate
+              ))
               .map((item, i) => {
                 return <li key={i}>{item.title}</li>;
-              })}
+            })}
           </ul>
         </div>
         <div id="TodayInOut">
