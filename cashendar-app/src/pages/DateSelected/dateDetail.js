ba715728502f8cde1c1553539dbe0006f863b660
5lft <zara0140@naver.com>
2022-11-21 01:28:16 +0900

    dateDetail 수정

--- cashendar-app/src/pages/DateSelected/dateDetail.js
+++ cashendar-app/src/pages/DateSelected/dateDetail.js
@@ -1,41 +1,73 @@
+import { issuedAtTime } from "@firebase/util";
 import React from "react";
+import { CarouselItem } from "react-bootstrap";
+import "./dateDetail.css";
 
-const DateDetail = (props) => {
+const DateDetail = ({ user, ScheduleDate, eventList, financeEList }) => {
     return (
-        <>
-            <div id="Detail">
-                <h3 id="TodayDate">Date: {props.ScheduleDate}</h3>
-                <div id="TodaySchedule">
-                    <h3 id="daySchedule">일정</h3>
-                    <ul>
-                        {props.eventList
-                            .filter(
-                                (item) =>
-                                    new Date(item.startD)
-                                        .toISOString()
-                                        .slice(0, 10) <= props.ScheduleDate &&
-                                    new Date(item.endD)
-                                        .toISOString()
-                                        .slice(0, 10) >= props.ScheduleDate
-                            )
-                            .map((item, i) => {
-                                return <li key={i}>{item.title}</li>;
-                            })}
-                    </ul>
-                </div>
-                <div id="TodayInOut">
-                    <h3 id="dayInOut">수입과 지출</h3>
-                    <ul>
-                        {props.financeEList
-                            .filter((item) => item.date === props.ScheduleDate)
-                            .map((item, i) => {
-                                return <li key={i}>{item.title}</li>;
-                            })}
-                    </ul>
+        <div id="Detail">
+            <p id="TodayDate">{ScheduleDate}</p>
+            <div id="TodaySchedule">
+                <div class="list-group" id="daySchedule">
+                    <a
+                        href="#"
+                        class="list-group-item list-group-item-action active"
+                    >
+                        일정
+                    </a>
+                    {eventList
+                        .filter(
+                            (item) =>
+                                new Date(item.startD)
+                                    .toISOString()
+                                    .slice(0, 10) <= ScheduleDate &&
+                                new Date(item.endD)
+                                    .toISOString()
+                                    .slice(0, 10) >= ScheduleDate
+                        )
+                        .map((item, i) => {
+                            return (
+                                <li
+                                    key={i}
+                                    class="list-group-item list-group-item-action disabled"
+                                >
+                                    {item.title}
+                                </li>
+                            );
+                        })}
                 </div>
             </div>
-            ;
-        </>
+            <div id="TodayInOut">
+                <div class="list-group" id="daySchedule">
+                    <a
+                        id="dayInOut"
+                        href="#"
+                        class="list-group-item list-group-item-action active"
+                    >
+                        수입 및 지출
+                    </a>
+                    {financeEList
+                        .filter((item) => item.date === ScheduleDate)
+                        .map((item, i) => {
+                            return (
+                                <li
+                                    key={i}
+                                    class="list-group-item list-group-item-action disabled"
+                                >
+                                    {item.isPlus ? (
+                                        <p class="item_amount">{item.title}</p>
+                                    ) : (
+                                        <p class="item_amount" id="plus">
+                                            {item.title}
+                                        </p>
+                                    )}
+                                    <p class="item_title">{item.titleF}</p>
+                                </li>
+                            );
+                        })}
+                </div>
+            </div>
+        </div>
     );
 };
 
